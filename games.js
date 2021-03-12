64a042325e4fcb38ff71f805224cd4a42e4b8ad4
Andre-MR-Pereira <andregdpereira@gmail.com>
2021-03-12 17:34:45 +0000

    Game done.Cleaning needed

--- games.js
+++ games.js
@@ -1,55 +1,112 @@
 var moves = 0;
 var picks = [];
+var ids = [];
 var pairs = 0;
+var cards = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11];
 initial_time = Date.now();
+final_time = 0;
+final_moves = 0;
 
-
-function increment_move(number) {
-    console.log(number)
-    moves += 1;
-    document.getElementById("movimentos").innerHTML = moves;
-    picks.push(number);
-    if (picks.length == 2) {
-        console.log("FAZ PAR")
-        console.log(picks)
-        if (picks[0] == picks[1]) {
-            matched = true;
-            pairs++;
-        } else {
-            matched = false;
+function increment_move(card, id) {
+    if (ids.length == 0 || (ids.length == 1 && id != ids[0])) {
+        moves += 1;
+        document.getElementById("movimentos").innerHTML = moves;
+        picks.push(card);
+        ids.push(id);
+        if (picks.length == 2) {
+            if (picks[0] == picks[1]) {
+                matched = true;
+                pairs++;
+            } else {
+                matched = false;
+            }
+            board_state(matched, pairs, ids);
+            picks = [];
+            ids = [];
         }
-        board_state(matched, picks);
-        picks = [];
     }
 }
 
-function board_state(matched) {
-
+async function board_state(matched, pairs, ids) {
+    if (matched == true) {
+        var item = document.getElementById(ids[0]);
+        item.classList.toggle("flipped");
+        item.classList.add("paired");
+        item.removeEventListener('click', transform);
+        item.removeEventListener('click', increment_move);
+        var item = document.getElementById(ids[1]);
+        item.classList.toggle("flipped");
+        item.classList.add("paired");
+        item.removeEventListener('click', transform);
+        item.removeEventListener('click', increment_move);
+    } else {
+        await sleep(2000);
+        transform(ids[0]);
+        transform(ids[1]);
+    }
+    if (pairs == 12) {
+        clearInterval(temporizador);
+        final_time = Date.now() - initial_time;
+        final_moves = moves;
+        var item = document.getElementsByClassName("stats");
+        item[item.length - 1].innerHTML += '<div class="final">RESULTADOS FINAIS</div>';
+        document.getElementById("tempo").innerHTML = timeToString(final_time);
+        document.getElementById("movimentos").innerHTML = final_moves;
+        document.getElementById("legenda_tempo").innerHTML = "";
+    }
 }
 
 function start_card_game() {
-    var cards = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11];
     shuffle(cards);
     var i = 0;
     for (var i = 0; i < cards.length / 3; i++) {
         document.getElementById('game').innerHTML += '<div class="row" name="row">';
         for (var j = 0; j < 3; j++) {
             var item = document.getElementsByName("row");
-            item[item.length - 1].innerHTML += '<div class="column"><div class="flip-card"><div class="flip-card-inner" name="bloco">';
-            var item = document.getElementsByName("bloco");
-            item[item.length - 1].innerHTML += '<div class="card-front"><p>NEEC</p></div>';
-            item[item.length - 1].innerHTML += '<div class="card-back"><img src="./assets/cards/0.png" alt="Avatar" style="width:100%" name="img"></div>';
+            item[item.length - 1].innerHTML += '<div class="column"><div class="card">';
+            var item = document.getElementsByClassName("card");
+            item[item.length - 1].id = (i * 3) + j;
+            item[item.length - 1].innerHTML += '<p name="marca" id="p0">NEEC</p>';
+            item[item.length - 1].innerHTML += '<img src="./assets/cards/0.png" style="display: none;" id="i0" name="img">';
             var item = document.getElementsByName("img");
             item[item.length - 1].src = "./assets/cards/" + cards[(i * 3) + j] + ".png";
             var item = document.getElementsByName("row");
-            item[item.length - 1].innerHTML += '</div></div></div>';
+            item[item.length - 1].innerHTML += '</div></div>';
         }
         document.getElementById('game').innerHTML += '</div>';
     }
-    var cardx = document.getElementsByClassName("card-back");
+    var item = document.getElementsByName("marca");
     for (var i = 0; i < cards.length; i++) {
-        cardx[i].addEventListener("onclick", displayCard);
-        cardx[i].addEventListener("onclick", increment_move(cards[i]));
+        item[i].id = "p" + i;
+    }
+    var item = document.getElementsByName("img");
+    for (var i = 0; i < cards.length; i++) {
+        item[i].id = "i" + i;
+    }
+
+    document.querySelectorAll('.card').forEach(item => {
+        item.addEventListener('click', event => {
+            transform(item.id)
+            increment_move(cards[item.id], item.id)
+        })
+    });
+}
+
+function transform(id) {
+    var item = document.getElementById(id);
+    item.classList.toggle("card");
+    item.classList.toggle("flipped");
+    var item = document.getElementById("p" + id);
+    if (item.style.display === "none") {
+        item.style.display = "block";
+    } else {
+        item.style.display = "none";
+    }
+    var item = document.getElementById("i" + id);
+    if (item.style.display === "none") {
+        item.style.display = "block";
+    } else {
+        item.style.display = "none";
     }
 }
 
@@ -66,16 +123,35 @@ function shuffle(array) {
     return array;
 }
 
-setInterval(function printTime() {
+var temporizador = setInterval(function printTime() {
     let elapsedTime = Date.now() - initial_time;
     document.getElementById("tempo").innerHTML = Math.floor(elapsedTime / 1000);
 }, 1000);
 
-var displayCard = function () {
-    /*this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");*/
-    console.log("Display");
-};
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMiliSec = (diffInSec - ss) * 10000;
+    let ms = Math.floor(diffInMiliSec);
+
+    let formattedHH = hh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+
+    return `${formattedMM}:${formattedSS}:${ms}`;
+}
 
 document.getElementById("movimentos").innerHTML = moves;
+document.getElementById("legenda_tempo").innerHTML = " segundos";
+
